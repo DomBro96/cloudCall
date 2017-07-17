b54c67fad4ffea73dae31ddfc65a4f42d4c11732
CaoLe <cleeeee@qq.com>
2017-07-17 14:18:42 +0800

    delete session.close();

--- src/main/java/cn/dombro/cloudCall/dao/info/Impl/EnterpriseCustomerMapperImpl.java
+++ src/main/java/cn/dombro/cloudCall/dao/info/Impl/EnterpriseCustomerMapperImpl.java
@@ -35,7 +35,6 @@ public class EnterpriseCustomerMapperImpl implements EnterpriseCustomerMapper{
         try {
 
         }finally {
-            session.close();
         }
         return null;
     }
